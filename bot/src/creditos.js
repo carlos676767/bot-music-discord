c36e7f0cd38abb97c750f9465e302f80d94ecab4
carlos676767 <carlitobr730@gmail.com>
2024-05-30 13:57:16 -0700

    ok

--- bot/src/creditos.js
+++ bot/src/creditos.js
@@ -11,31 +11,26 @@ const client = new Client({
 });
 
 
-
-const botoes = () => {
+const botaoInstagram = () => {
     const linkUrl = new ButtonBuilder()
         .setLabel("Instagram")
         .setURL("https://www.instagram.com/chenrique_dev/")
         .setStyle(ButtonStyle.Link)
-    const row = new ActionRowBuilder().addComponents(linkUrl);
+    const Github = new ButtonBuilder()
+        .setLabel("GitHub")
+        .setURL("https://github.com/carlos676767?")
+        .setStyle(ButtonStyle.Link)
+    const row = new ActionRowBuilder().addComponents(linkUrl, Github);
     return row
 }
 
+
+
+
 function creditos() {
-    const creditos = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(`\nCréditos do Bot visionario`)
-        .setThumbnail('https://imgur.com/SVZdl7m.png')
-        .setDescription(`Somos seu companheiro perfeito 
-        para todas as suas necessidades musicais! Com uma 
-        vasta gama de funcionalidades, estamos aqui para tornar 
-        sua experiência musical excepcional. Basta digitar !menu 
-        para abrir nosso menu e explorar todas as opções emocionantes 
-        que oferecemos.`)
-        .addFields({ name: 'Instagram', value: '@chenrique_dev' }, { name: "Github", value: 'carlos67676' })
     client.on("messageCreate", (msg) => {
         if (msg.content == "!creditos") {
-            msg.channel.send({ embeds: [creditos], components: [botoes()] });
+            msg.channel.send({ embeds: [creditos], components: [botaoInstagram()] });
         }
     })
 }
