7e442b1c26b21b38609c52c060e3af296556caf2
carlos676767 <carlitobr730@gmail.com>
2024-05-31 10:32:09 -0700

    ok

--- bot/src/jokenpo.js
+++ bot/src/jokenpo.js
@@ -17,12 +17,34 @@ const randomTesoura = () => {
     return pegarJogo
 }
 
+const jogarPedraPapelTesoura = (msg, comando) => {
+    const randomGame = randomTesoura()
+    if (comando == 'pedra' && randomGame == 'tesoura') {
+        msg.reply(`voce jogou ${comando} e o computador ${randomGame}, parabens voce ganhou`)
+        return
+    }
+    if (comando == 'tesoura' && randomGame == 'papel') {
+        msg.reply(`voce jogou ${comando} e o computador ${randomGame}, parabens voce ganhou`)
+        return
+    }
+    if (comando == 'papel' && randomGame == 'pedra') {
+        msg.reply(`voce jogou ${comando} e o computador ${randomGame}, parabens voce ganhou`)
+        return
+    } 
+    if (randomGame === comando) {
+        msg.reply(`voce jogou ${comando} e o computador ${randomGame}, empate`)
+    }else {
+        msg.reply(`voce jogou ${comando} e o computador ${randomGame},  voce perdeu`)
+    }
+
+}
 
 
 const jogar = () => {
     client.on("messageCreate", msg => {
         if (msg.content.includes("!jogar")) {
             const comando = msg.content.slice(7, Infinity)
+            jogarPedraPapelTesoura(msg, comando)
         }
     })
 }
